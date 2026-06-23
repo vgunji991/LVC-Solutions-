ab400f92af1b22029e7234dbe846f9c81c7fd0bd
TKChandan28 <kthatavarthi@lvc-solutions.com>
2026-06-23 10:49:14 +0530

    Final changes for css in ouremployees

--- frontend/src/components/ourEmployees.js
+++ frontend/src/components/ourEmployees.js
@@ -5,38 +5,36 @@ const employees = [
     id: 1,
     image: "/employee_pics/Venkat_ceo.jpeg", 
     name: "Venkat Gunji",
-    position: "CEO",
-    Role: "Founder",
+    position: "Founder & CEO",
+
   },
   {
     id: 2,
     image: "/employee_pics/teja.jpg", 
     name: "Teja",
-    position: "Intern",
-    Role: "Full Stack Developer",
+   position: "Full Stack  Intern",
   },
   {
     id: 3,
     image: "/employee_pics/vamshi.jpeg",
     name: "A Bhanu Vamshi",
-    position: "Intern",
-    Role: "Full Stack Developer",
+    position: "Full Stack  Intern",
+   
   },
   
   {
     id: 4,
     image: "/employee_pics/chandan.jpeg", 
     name: "T.K.Chandan",
-    position: "Intern",
-    Role: "Full Stack Developer",
+     position: "Full Stack Intern",
   }
   ,
   {
     id: 5,
     image: "/employee_pics/chakresh.jpeg", 
     name: "Chakresh",
-    position: "Intern",
-    Role: "Full Stack Developer",
+   position: "Full Stack Intern",
+   
   }
 ];
 
@@ -56,9 +54,9 @@ const OurEmployees = () => {
           {[...employees, ...employees].map((employee, index) => (
             <div className="employee-card" key={index}>
               <img src={employee.image} alt={employee.name} />
-              <h4>{employee.name}</h4>
-              <h4>{employee.position}</h4>
-              <h4>{employee.Role}</h4>
+              <h4 style={{ fontSize: "13px" }}>{employee.name}</h4>
+              <h4 style={{ fontSize: "12px" }} >{employee.position}</h4>
+              {/* <h4 style={{ fontSize: "12px" }}>{employee.Role}</h4> */}
             </div>
           ))}
         </div>
